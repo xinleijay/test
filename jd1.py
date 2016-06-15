64e04bf1cca11d5547d379188bd1ba96233bb9ce
hulibal <huzgde@gmail.com>
2016-06-15 11:59:30 +0800

    add some content

--- jd1.py
+++ jd1.py
@@ -1,6 +1,6 @@
 ﻿#第一行输入学习天数和总的时间 第二行到第day+1行输入每天他爸要求他学习的最短时间和最长时间。
 #求问他学习的天数和总时间能满足他爸每天的要求吗？yes的话则输出每天的学习时间。
-
+#京东笔试题
 from math import *
 while True:
     (day,sumtime)=(int(x) for x in raw_input().split())
